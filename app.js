b0c10e6c5c7f511650d72e2cbf317bd79460ca76
Numan Nusrullah <numansahil817@gmail.com>
2019-04-02 23:58:55 +0500

    connecting db

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
 
-require('./server/config/passport-config')(server, users);
+require('./server/config/passport-config')(server);
 require('./server/routes/all-routes')(server);
 
 server.use((err, req, res, next) => {
